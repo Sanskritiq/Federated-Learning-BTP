365be9b74c5a522ab81652eb7a37b18a50e98623
Sanskriti Singh <sanskriti180903@gmail.com>
2023-11-21 14:15:22 +0530

    [.] corrected fedprox

--- Paper_Implementation/client/trainer.py
+++ Paper_Implementation/client/trainer.py
@@ -15,37 +15,50 @@ from functions.serialization import Serialization
 
 class ClientTrainer:
     def __init__(self, client_id, global_model:nn.Module, train_loader:DataLoader) -> None:
+        # self.global_model = global_model
         self.client_id = client_id
         self.model = deepcopy(global_model)
         self.train_loader = train_loader
         self.iterator = iter(self.train_loader)
         self.lr = client_lr
-        self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
+        self.optimizer = optim.SGD(self.model.parameters(), lr=self.lr)
         self.loss = nn.CrossEntropyLoss()
         self.epochs = client_epochs
         self.loss_history = []
         
-    def train(self, serialized_global_params:torch.Tensor = None):
+    def train(self, serialized_global_params: torch.Tensor = None):
         if serialized_global_params is not None:
             Serialization.deserialize(self.model, serialized_global_params)
         self.model.train()
-        
+
         for _ in trange(self.epochs, desc="client [{}]".format(self.client_id)):
             x, y_true = self.trainLoaderIterator()
-            y_pred = self.model(x) 
+            y_pred = self.model(x)
             loss_value = self.loss(y_pred, y_true)
-            if aggregator_method=='fedprox':
+
+            if aggregator_method == 'fedprox':
+                # Initialize gradients
+                self.optimizer.zero_grad()
                 # Add proximal term to loss (FedProx)
                 w_diff = torch.tensor(0.)
-                for w, w_t in zip(self.model.parameters(), self.global_model.parameters()):
-                    w_diff += torch.pow(torch.norm(w.data - w_t.data), 2)
-                    w.grad.data += proximal_coff * (w_t.data - w.data)
+                for w, w_t in zip(self.model.parameters(), serialized_global_params):
+                    w_diff += torch.pow(torch.norm(w.data - w_t), 2)
+                    # Create a dummy tensor that depends on w to perform backward pass
+                    dummy_loss = torch.sum(w)
+                    dummy_loss.backward()
+                    # Now w.grad should not be None
+                    w.grad.data += proximal_coff * (w_t - w.data)
                 loss_value += proximal_coff / 2. * w_diff
+
             loss = loss_value.item()
             self.loss_history.append(loss)
             self.optimizer.zero_grad()
             loss_value.backward()
             self.optimizer.step()
+
+            # Ensure w.grad is not None after backward pass
+            for w in self.model.parameters():
+                assert w.grad is not None, "Gradient is None after backward pass!"
         
     def trainLoaderIterator(self):
         try:
